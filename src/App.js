ef46db7844fc8b19ba0b12da4712d4fd55e8e91d
mehdi-mtir <mehdi.mtir@gmail.com>
2023-11-20 15:45:09 +0100

    list of comments avec map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import Comment from './components/Comment';
 
 function App() {
+  let comments = [
+    {
+      nom : "Stevie Feliciano",
+      photo : "https://semantic-ui.com/images/avatar/small/stevie.jpg",
+      date_publication : "17/11/2023",
+      evaluation : "5",
+      texte : "Hey guys, I hope this example comment is helping you read this documentation."
+    },
+    {
+      nom : "Tom Lukic",
+      photo : "https://semantic-ui.com/images/avatar/small/joe.jpg",
+      date_publication : "19/11/2023",
+      evaluation : "4",
+      texte : "This will be great for business reports. I will definitely download this."
+    },
+    {
+      nom : "Steve Jobes",
+      photo : "https://semantic-ui.com/images/avatar/small/steve.jpg",
+      date_publication : "20/11/2023",
+      evaluation : "3",
+      texte : "Revolutionary!"
+    }
+  ]
   return (
   <>
-    <Comment nom="Stevie Feliciano" photo="https://semantic-ui.com/images/avatar/small/stevie.jpg" />
-    <Comment nom="Tom Lukic" photo="https://semantic-ui.com/images/avatar/small/joe.jpg" />
-    <Comment nom="Steve Jobes" photo="https://semantic-ui.com/images/avatar/small/steve.jpg" />
+    {
+    comments.map(comment => <Comment
+                              nom={comment.nom}
+                              photo={comment.photo}
+                              date_publication={comment.date_publication}
+                              evaluation={comment.evaluation}
+                              texte = {comment.texte} />
+                )
+    }
   </>
   );
 }
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,22 +1,22 @@
 
-const Comment = (props)=>{
+const Comment = ({photo, nom, date_publication, evaluation, texte})=>{
   return (
   <div className="ui comments">
   <div className="comment">
     <a className="avatar">
-      <img src={props.photo} />
+      <img src={photo} />
     </a>
     <div className="content">
-      <a className="author">{props.nom}</a>
+      <a className="author">{nom}</a>
       <div className="metadata">
-        <div className="date">2 days ago</div>
+        <div className="date">{date_publication}</div>
         <div className="rating">
           <i className="star icon"></i>
-          5 Faves
+          {evaluation} Faves
         </div>
       </div>
       <div className="text">
-        Hey guys, I hope this example comment is helping you read this documentation.
+        {texte}
       </div>
     </div>
   </div>
